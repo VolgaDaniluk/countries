28b5e8ff5bb51dea9f84aa379ba9c3ab68d9be30
Volha Daniluk <olgadaniluk.i@gmail.com>
2024-03-14 12:49:58 +0100

    add dependencies versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ plugins {
     java
     id("org.springframework.boot") version "3.2.3"
     id("io.spring.dependency-management") version "1.1.4"
-    id("org.flywaydb.flyway") version "9.8.1"
+    id("org.flywaydb.flyway") version "10.10.0"
 }
 
 group = "com.danliuk"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
@@ -21,12 +21,12 @@ dependencies {
     implementation("org.postgresql:postgresql")
 
 //    OpenApi
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 
 //	MapStruct
-    implementation("org.mapstruct:mapstruct")
-    annotationProcessor("org.mapstruct:mapstruct-processor")
-    annotationProcessor("org.projectlombok:lombok-mapstruct-binding")
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 
 //    Lombok
     compileOnly("org.projectlombok:lombok")
@@ -34,7 +34,7 @@ dependencies {
 
 //	flyway
     implementation("org.flywaydb:flyway-core")
-    runtimeOnly("org.flywaydb:flyway-gradle-plugin")
+    runtimeOnly("org.flywaydb:flyway-gradle-plugin:10.9.1")
 
 //    Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
